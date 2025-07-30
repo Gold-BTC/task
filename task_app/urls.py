2fc6db8aff74f64b74b5dce8769c789865ca9fda
gold_BTC <scherbina.mihail2010@gmail.com>
2025-07-30 17:21:56 +0300

    q

---
+++ task_app/urls.py
@@ -0,0 +1,9 @@
+# urls.py
+from django.urls import path
+from task_app import views
+
+urlpatterns = [
+    path('', views.TaskListView.as_view(), name='task_list'),
+    path('task/<int:pk>', views.TaskDetailView.as_view(), name='task_list')
+
+]
--- task_app/views.py
+++ task_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from task_app.models import Task
 
-# Create your views here.
+
+class TaskListView(ListView):
+    model = Task
+    template_name = 'task_list.html'
+
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'task_detail.html'
