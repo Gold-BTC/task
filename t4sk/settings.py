f48a11ca218ff525726de65c64a6cec74b2904c4
gold_BTC <scherbina.mihail2010@gmail.com>
2025-08-14 13:05:09 +0300

    .

--- t4sk/settings.py
+++ t4sk/settings.py
@@ -122,7 +122,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 LOGIN_URL = '/login/'
-
+LOGIN_
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
